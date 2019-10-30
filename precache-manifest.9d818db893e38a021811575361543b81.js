b06e9281a183fa4c7e4d81d1b842ec3e4a84ebd4
klodkup340 <sagun.phetkaew@gmail.com>
2019-10-30 18:39:41 +0700

    Updates

---
+++ precache-manifest.9d818db893e38a021811575361543b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513cf175f70c145bcc5f48869ef31cf0",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b96cb08cc693cda9fcc0",
+    "url": "/test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/test/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "b96cb08cc693cda9fcc0",
+    "url": "/test/static/js/main.1c05cc9a.chunk.js"
+  },
+  {
+    "revision": "35bd43fe6303cc8fd418",
+    "url": "/test/static/js/runtime-main.04f6b747.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test/static/media/logo.25bf045c.svg"
+  }
+]);
